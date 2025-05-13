dae6609390993b6ba5e8b7935b782cc49b30ae2f
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-13 15:05:52 +0200

    [UPD] Card Structure + hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  const endpoint = "https://lanciweb.github.io/demo/api/actors/";
-  const [actor, setActor] = useState([]);
+  const endpoint = "https://lanciweb.github.io/demo/api/actors";
+  const [actors, setActors] = useState([]);
 
-  const fetchActor = () => {
+  //Fetch API
+  const fetchActors = () => {
     axios
       .get(endpoint)
       .then((response) => {
-        setActor(response.data.results);
+        setActors(response.data);
       })
-      .error((error) => console.error(error));
+      .catch((error) => console.error(error));
   };
-  return <></>;
+  //useEffect for fetching data when loading
+  useEffect(fetchActors, []);
+  return (
+    <>
+      <div className="container py-4">
+        <nav className="mb-4">
+          <h1 className="text-center">Cast Fetching</h1>
+        </nav>
+        <div className="row g-4">
+          {actors.map((actor) => (
+            <div key={actor.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+              <div className="card shadow-sm h-100">
+                <img
+                  src={actor.image}
+                  alt=""
+                  className="card-img-top mt-2"
+                  style={{ objectFit: "contain", height: "250px" }}
+                />
+                <div className="card-body">
+                  <div className="actor-name"></div>
+                  <div className="actor-birth"></div>
+                  <div className="actor-nationality"></div>
+                  <div className="actor-bio"></div>
+                  <div className="actor-riconoscimenti"></div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
